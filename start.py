b66830b161ca99e120b40fdd6e2a5cca12f6c06a
underex <107438973+underex@users.noreply.github.com>
2022-06-18 02:04:43 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,15 @@
+# import os
+# import sys 
+
+# print("Запуск!")
+
+# if os.sys.platform == "win32":
+#     os.system("cls")
+# else:
+#     os.system("clear")
+    
+    
+    
+# os.system()
+
+import StarZed
